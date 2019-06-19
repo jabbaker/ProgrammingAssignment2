a81b8a5ed64aee852a711fd93d47c179a94ce910
jabbaker <jeffbaker6791@gmail.com>
2019-06-19 14:15:02 -0400

    Final file with instructions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do 
 
-## Write a short comment describing this function
+## instructions 
+
+## 
+## Load this file (cachematrix.R) into RStudio, then run the file to load the functions into the env
+## How to create a sample 10x10 random matrix: matrixName <- matrix(sample.int(15, 10*10, TRUE), 10, 10)
+## Type: myMatrix <- makeCacheMatrix(matrixName) into RStudio
+## Type: cacheSolve(myMatrix) once to cache the inverse of MatrixName
+## Type: cacheSolve(myMatrix) a 2nd time to retrieve MatrixName from the cache
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-        print(x)
+        m <- NULL
+                set <- function(y) {
+                        x <<- y
+                        m <<- NULL                                      
+                }
+        get <- function() x                               
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+        setsolve = setsolve,
+        getsolve = getsolve)       
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve 
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
